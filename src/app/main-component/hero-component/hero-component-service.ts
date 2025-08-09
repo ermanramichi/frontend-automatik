c9e9752593697e4986f882363d3a03807e327631
Erman Ramichi <105870449+ermanramichi@users.noreply.github.com>
2025-08-09 19:54:29 +0200

    Update hero-component-service.ts

--- src/app/main-component/hero-component/hero-component-service.ts
+++ src/app/main-component/hero-component/hero-component-service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of, tap } from 'rxjs';
-import { environment } from '../../../environments/environment.prod';
+import { environment } from '../../../environments/environments.prod';
 export interface Quote {
   quote: string;
   author: string;
