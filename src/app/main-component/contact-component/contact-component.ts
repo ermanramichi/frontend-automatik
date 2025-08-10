696c874708bee47fc319ede261e0da320028da92
ermanramichi <ermanramiqi@icloud.com>
2025-08-10 03:48:20 +0200

    final update

--- src/app/main-component/contact-component/contact-component.ts
+++ src/app/main-component/contact-component/contact-component.ts
@@ -1,23 +1,30 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, NgModule } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import {FormsModule} from '@angular/forms'
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms'
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-contact-component',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule,HttpClientModule, FormsModule],
   templateUrl: './contact-component.html',
   styleUrl: './contact-component.css'
 })
 export class ContactComponent {
-  
+  contactForm: FormGroup;
+  isSubmitting = false;
+  submitted = false;
+  submitError = false; 
 
- 
-constructor(private route: ActivatedRoute){}
-  
+  private formspreeUrl = 'https://formspree.io/f/xanbdlar'
+constructor(private route: ActivatedRoute,private fb: FormBuilder,private http: HttpClient){
+this.contactForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required]
 
-    
- 
+});
+}
+  
 
   ngAfterViewInit(): void {
     this.route.fragment.subscribe(fragment => {
@@ -30,5 +37,41 @@ constructor(private route: ActivatedRoute){}
         }
       }
     });
+  }
+  onSubmit() {
+    if (this.contactForm.valid) {
+      this.isSubmitting = true;
+      this.submitted = false;
+      this.submitError = false;
+
+      const formData = {
+        email: this.contactForm.get('email')?.value,
+        subject: this.contactForm.get('subject')?.value,
+        message: this.contactForm.get('message')?.value,
+        _replyto: this.contactForm.get('email')?.value
+      };
+
+      this.http.post(this.formspreeUrl, formData, {
+        headers: {
+          'Accept': 'application/json'
+        }
+      }).subscribe({
+        next: (response) => {
+          this.isSubmitting = false;
+          this.submitted = true;
+          this.contactForm.reset();
+        },
+        error: (error) => {
+          this.isSubmitting = false;
+          this.submitError = true;
+          console.error('Form submission error:', error);
+        }
+      });
+    } else {
+      // Mark all fields as touched to show validation errors
+      Object.keys(this.contactForm.controls).forEach(key => {
+        this.contactForm.get(key)?.markAsTouched();
+      });
+    }
   }
 }
---
+++ src/environments/enviroment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiNinjasKey: 'zp8TlpTJtnx4f8gc5457Nw==JrksdoKvs1n8RrBF'
+};
