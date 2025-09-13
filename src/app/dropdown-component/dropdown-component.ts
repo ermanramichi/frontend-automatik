a306b16de65a40ef37ca6448c08a8e3a4f3eb8bc
Erman Ramichi <ermanramiqi@icloud.com>
2025-09-13 20:15:17 +0200

    new responsive fixes

--- src/app/dropdown-component/dropdown-component.ts
+++ src/app/dropdown-component/dropdown-component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
 import { MainCategoriesComponent } from "../categories/main-categories-component/main-categories-component";
 
 @Component({
@@ -7,21 +7,61 @@ import { MainCategoriesComponent } from "../categories/main-categories-component
   templateUrl: './dropdown-component.html',
   styleUrl: './dropdown-component.css'
 })
-export class DropdownComponent {
+export class DropdownComponent implements OnInit{
   @Input() dropDownActive!:boolean;
   @Output() notActive = new EventEmitter<boolean>();
+  screenWidth = 0;
+  isMobile = false;
+  isTablet = false;
+  isDesktop = false;
+  isLargeDesktop=false;
+  deviceType = '';
 
+
+
+  ngOnInit() {
+    this.updateScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.updateScreenSize();
+  }
+
+  private updateScreenSize() {
+    this.screenWidth = window.innerWidth;
+
+    // Define breakpoints
+    this.isMobile = this.screenWidth < 768;
+    this.isTablet = this.screenWidth >= 768 && this.screenWidth < 1024;
+    this.isDesktop = this.screenWidth >= 1024 && this.screenWidth<1920;
+    this.isLargeDesktop=this.screenWidth>=1920;
+
+    // Set device type for debugging
+    if (this.isMobile) this.deviceType = 'Mobile';
+    else if (this.isTablet) this.deviceType = 'Tablet';
+    else this.deviceType = 'Desktop';
+  }
   get baseClasses():string{
 
-    const baseClasses = 'drop-down w-full h-[600px] bg-white fixed top-24 left-0 flex items-start justify-start shadow-xl transition-all duration-300 ease-in-out';
-
-    if (this.dropDownActive) {
+    if(this.isMobile){
+      const baseClasses = 'drop-down w-full h-full bg-white fixed top-32 py-2 left-0 flex items-start justify-start shadow-xl transition-all duration-300 ease-in-out';
+      if (this.dropDownActive) {
       return `${baseClasses} z-[9998] opacity-100 translate-y-0`;
     }
 
-    return `${baseClasses} z-[9998] opacity-0 -translate-y-4 pointer-events-none`;
+      return `${baseClasses} z-[9998] opacity-0 translate-y-4 pointer-events-none`;
 
-  }
+    }
+    const baseClasses = 'drop-down w-full h-[600px] bg-white fixed top-24 left-0 flex items-start justify-start shadow-xl transition-all duration-300 ease-in-out';
+      if (this.dropDownActive) {
+      return `${baseClasses} z-[9998] opacity-100 translate-y-0`;
+    }
+
+      return `${baseClasses} z-[9998] opacity-0 translate-y-4 pointer-events-none`;
 
 
+
+
+}
 }
