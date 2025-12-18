073526f1fdc2aeb2370408f59604e68fd9acfd17
Erman Ramichi <105870449+ermanramichi@users.noreply.github.com>
2025-12-18 01:18:30 +0100

    fix product not showing

--- src/app/product-details-component/product-details-component.ts
+++ src/app/product-details-component/product-details-component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit, SimpleChanges } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, SimpleChanges } from '@angular/core';
 import { ProductPriceService } from '../services/product-price-service/product-price-service';
 import { ProductService } from '../services/product-service/product-service';
 import { ActivatedRoute } from '@angular/router';
 import { StoresService } from '../services/stores-service/stores-service';
+import { error } from 'console';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-details-component',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule],
   templateUrl: './product-details-component.html',
   styleUrl: './product-details-component.css',
 })
 export class ProductDetailsComponent implements OnInit {
-  productId!:string;
+  productId: string | null = null;
   product: any;
   prices: any[]=[];
 
@@ -19,33 +22,51 @@ export class ProductDetailsComponent implements OnInit {
     private route: ActivatedRoute,
     private productService: ProductService,
     private productPriceService: ProductPriceService,
-    private storesService: StoresService
+    private storesService: StoresService,
+    private cdr: ChangeDetectorRef
   ) { }
+  
   ngOnInit(): void {
-    this.productId = this.route.snapshot.paramMap.get('id')!;
-    this.loadProduct();
-    this.loadPrices();
-  }
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['productId']) {
+  this.route.paramMap.subscribe(params => {
+    const id = params.get('id');
+    if (id) {
+      this.productId = id;
+      this.product = null; 
+      this.prices = [];
       this.loadProduct();
       this.loadPrices();
     }
-  }
+  });
+}
+  
   loadProduct() {
-    this.productService.getProductsById(this.productId).subscribe(products=>{
-      this.product=products[0];
-    });
+    this.productService.getProductsById(this.productId!).subscribe({
+    next:(data)=>{
+      this.product=data[0];
+      this.cdr.detectChanges();
+      console.log(this.product);
+    },
+    error:(err)=>{
+      console.error('Error fetching product details:', err);
+    }
+  });  
   }
+  
   loadPrices() {
-    this.productPriceService.getByProductId(this.productId).subscribe(prices=>{
-      this.prices=prices;
-      this.prices.forEach(price=>{
-        this.storesService.getStorebyID(price.StoreID).subscribe(store=>{
-          price.storeName=store?.Name;
-          price.storeLogoUrl=store?.LogoUrl;
-        });
+  this.productPriceService.getByProductId(this.productId!).subscribe(prices => {
+    
+    this.prices = prices;
+    console.log(this.prices);
+  
+    this.prices.forEach((price, index) => {
+      this.storesService.getStorebyID(price.StoreID).subscribe(store => {
+        price.storeName = store?.Name;
+        price.storeLogoUrl = store?.LogoUrl;
+        this.cdr.detectChanges();
+        
+        this.prices = [...this.prices];
       });
     });
-  }
+  });
+}
 }
