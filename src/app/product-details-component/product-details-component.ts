6627e4536d689cb5ffd4093eb1928779669ee524
Erman Ramichi <105870449+ermanramichi@users.noreply.github.com>
2025-12-20 03:50:48 +0100

    product details

--- src/app/product-details-component/product-details-component.ts
+++ src/app/product-details-component/product-details-component.ts
@@ -1,3 +1,5 @@
+import { Head } from './../../../node_modules/rxjs/src/internal/types';
+import { SubCategoriesService } from './../services/sub-categories-service/sub-categories-service';
 import { ChangeDetectorRef, Component, OnInit, SimpleChanges } from '@angular/core';
 import { ProductPriceService } from '../services/product-price-service/product-price-service';
 import { ProductService } from '../services/product-service/product-service';
@@ -5,6 +7,7 @@ import { ActivatedRoute } from '@angular/router';
 import { StoresService } from '../services/stores-service/stores-service';
 import { error } from 'console';
 import { CommonModule } from '@angular/common';
+import { concat } from 'rxjs';
 
 @Component({
   selector: 'app-product-details-component',
@@ -17,13 +20,15 @@ export class ProductDetailsComponent implements OnInit {
   productId: string | null = null;
   product: any;
   prices: any[]=[];
+  crossSaleSubCategories: any[]=[];
 
   constructor(
     private route: ActivatedRoute,
     private productService: ProductService,
     private productPriceService: ProductPriceService,
     private storesService: StoresService,
-    private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef,
+    private SubCategoriesService: SubCategoriesService
   ) { }
   
   ngOnInit(): void {
@@ -35,6 +40,7 @@ export class ProductDetailsComponent implements OnInit {
       this.prices = [];
       this.loadProduct();
       this.loadPrices();
+      this.getCrossSaleSubCategories(this.productId);
     }
   });
 }
@@ -69,4 +75,26 @@ export class ProductDetailsComponent implements OnInit {
     });
   });
 }
+getStars(rating: number): number[] {
+  return Array(5)
+    .fill(0)
+    .map((_, i) => i < rating ? 1 : 0);
 }
+getCrossSaleSubCategories(productId: string): void {
+  this.productService.getProductsById(productId).subscribe(products => {
+    const productByID= products[0];
+    if (!productByID) return;
+    const categoryID = productByID.HeadCategoryID+productByID.CategoryID;
+    console.log('Category ID for cross-sale:', categoryID);
+
+    this.SubCategoriesService.getSubCategories().subscribe(subcategories => {
+      this.crossSaleSubCategories = subcategories.filter(sub =>
+        sub.ParentID === categoryID &&
+        sub.ID !== productByID.SubCategoryID
+        
+      );
+      this.cdr.detectChanges();
+      console.log(this.crossSaleSubCategories);
+    });
+  });
+}}
